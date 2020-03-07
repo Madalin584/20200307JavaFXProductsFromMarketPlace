fed0788fbf8bb88f650cff1349c25de01a233b42
Madalin584 <madalinmadalin223@gmail.com>
2020-03-07 16:00:08 +0200

    Fixed

--- src/main/java/sample/dao/UserDAOImpl.java
+++ src/main/java/sample/dao/UserDAOImpl.java
@@ -1,6 +1,7 @@
 package sample.dao;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import sample.model.User;
 import sample.service.HibernateUtil;
 
@@ -10,7 +11,10 @@ public class UserDAOImpl implements UserDAO {
     @Override
     public User findByUsernameAndPassword(String username, String password) {
         String hql = "select u from User u where username = :username and password = :password";
-        return session.createQuery(hql, User.class).getSingleResult();
+        Query<User> query = session.createQuery(hql, User.class);
+        query.setParameter("username", username);
+        query.setParameter("password", password);
+        return query.getSingleResult();
 
     }
 }
