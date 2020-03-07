e3c0b40fff059edc7cf613954514c246c9460065
Madalin584 <madalinmadalin223@gmail.com>
2020-03-07 15:02:38 +0200

    Added UserService,UserDAOImpl,UserDAO

--- src/main/java/sample/model/User.java
+++ src/main/java/sample/model/User.java
@@ -21,12 +21,20 @@ public class User {
     @Column
     private String password;
     @Column
+    private String firstname;
+    @Column
+    private String lastname;
+    @Column
+    private String email;
+    @Column
     private String role;
 
-    public User(String username, String password, String role) {
+    public User(String username, String password, String firstname, String lastname, String email, String role) {
         this.username = username;
         this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
         this.role = role;
     }
-
 }
