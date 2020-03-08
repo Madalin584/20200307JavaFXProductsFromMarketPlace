4c0124f37c7a1ed1d912c7dd8f3160201dbf2421
Andra Stefana <andra.livadaru@gmail.com>
2020-03-08 13:29:42 +0200

    Implemented welcome interface

--- src/main/java/sample/service/HibernateUtil.java
+++ src/main/java/sample/service/HibernateUtil.java
@@ -49,7 +49,7 @@ public class HibernateUtil {
         settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
         settings.put(Environment.URL, "jdbc:mysql://localhost:3306/productsfrommarketplace_schema?useSSL=false");
         settings.put(Environment.USER, "root");
-        settings.put(Environment.PASS, "root");
+        settings.put(Environment.PASS, "A906090a");
         settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
         settings.put(Environment.SHOW_SQL, "true");
         settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
