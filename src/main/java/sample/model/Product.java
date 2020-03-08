36b88ba73467d6bccd67f30c2a8ea256a4554b2f
Andra Stefana <andra.livadaru@gmail.com>
2020-03-08 14:20:50 +0200

    Product class modified

--- src/main/java/sample/model/Product.java
+++ src/main/java/sample/model/Product.java
@@ -1,5 +1,41 @@
 package sample.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int price;
+
+    @Column
+    private int quantity;
+
+    @Column
+    private String description;
+
+    public Product(String name, int price, int quantity, String description) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
+    }
 }
